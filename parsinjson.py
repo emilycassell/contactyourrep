b66f6866ea82f602d199f4482b8fff547287957f
emilycassell <emily.cassell@columbia.edu>
2019-02-17 10:00:16 -0800

    final

--- parsinjson.py
+++ parsinjson.py
@@ -8,48 +8,55 @@ import os
 import requests
 import urllib.parse
 
-main_api = 'https://q4ktfaysw3.execute-api.us-east-1.amazonaws.com/treehacks/legislators?'
-address = 'California'
-level = 'NATIONAL_LOWER'
-url = main_api + urllib.parse.urlencode({'address': address})+urllib.parse.urlencode({'level': level})
-headers = { 'X-API-KEY': '2e1uvo7yeX50ZGHvctPxi8ZWubhggyOydIWvOa5c'}
+#main_api = 'https://q4ktfaysw3.execute-api.us-east-1.amazonaws.com/treehacks/legislators?'
+#address = 'California'
+#level = 'NATIONAL_LOWER'
+#url = main_api + urllib.parse.urlencode({'address': address})+urllib.parse.urlencode({'level': level})
+#headers = { 'X-API-KEY': '2e1uvo7yeX50ZGHvctPxi8ZWubhggyOydIWvOa5c'}
 
-json_data = requests.get(url, headers=headers).json()
+#json_data = requests.get(url, headers=headers).json()
 
 #data not 100% accruate ??
 
 
-for i in range(0,len(json_data['officials'])):
-	if (json_data['officials'][i]['office_details']['position'] == 'Representative') or (json_data['officials'][i]['office_details']['position'] == 'Senator') and json_data['officials'][i]['term_end']>"2019-01-01 00:00:00" :
-		print(json_data['officials'][i]['office_details']['position']+ ' ' + json_data['officials'][i]['first_name'] + ' ' + json_data['officials'][i]['last_name'])
-		print(json_data['officials'][i]['office_location']["phone_1"])
+#for i in range(0,len(json_data['officials'])):
+	#if (json_data['officials'][i]['office_details']['position'] == 'Representative') or (json_data['officials'][i]['office_details']['position'] == 'Senator') and json_data['officials'][i]['term_end']:
+		#print(json_data['officials'][i]['office_details']['position']+ ' ' + json_data['officials'][i]['first_name'] + ' ' + json_data['officials'][i]['last_name'])
+		#print(json_data['officials'][i]['office_location']["phone_1"])
 
 
 def create_response(data):
 	name=[]
 	phone=[]
-	for i in range(0,len(json_data['officials'])):
-		if (data['officials'][i]['office_details']['position'] == 'Representative') or (json_data['officials'][i]['office_details']['position'] == 'Senator') and json_data['officials'][i]['term_end']>"2019-01-01 00:00:00" :
-			name.append(json_data['officials'][i]['office_details']['position']+ ' ' + json_data['officials'][i]['first_name'] + ' ' + json_data['officials'][i]['last_name'] + ': '+ ' ' + json_data['officials'][i]['office_location']["phone_1"] + "\n\n")
+	for i in range(0,len(data['officials'])):
+		if (data['officials'][i]['office_details']['position'] == 'Representative') or (data['officials'][i]['office_details']['position'] == 'Senator') and data['officials'][i]['term_end']:
+			name.append(data['officials'][i]['office_details']['position']+ ' ' + data['officials'][i]['first_name'] + ' ' + data['officials'][i]['last_name'] + ': '+ ' ' + data['officials'][i]['office_location']["phone_1"] + "\n\n")
 			
 	return (name)
 
 
-contactdict = create_response(json_data)
+#contactdict = create_response(json_data)
 #print(contactdict)
 
-print(' '.join(str(x) for x in contactdict))
-#replytext = ("call your senator:" + " ".join(str(x) for x in contactdict))
-#print(replytext)
+#print(' '.join(str(x) for x in contactdict))
 
-#newfile = create_response(json_data)
-#newfilename = json.dumps(newfile["name_position"][2])
-#newfilenumber = json.dumps(newfile["phonenumber"][2])
 
-#for i in contact
+def ngrams(input, n):
+  input = input.split(' ')
+  output = []
+  for i in range(len(input)-n+1):
+    output.append(input[i:i+n])
+  return output
 
-#print("call your representatives today:" + newfilename[] + ' ' + newfilenumber)
-#print("call your senator")
+
+
+def combine_ngrams(thing):
+	newlist=[]
+	for i in thing:
+		s = " ".join(i)
+		newlist.append(s)
+	return newlist
+
 
 
 
--- redditexamplebot1.py
+++ redditexamplebot1.py
@@ -8,6 +8,9 @@ from keywords import keyworddict
 from textblob import TextBlob
 
 from parsinjson import create_response 
+from parsinjson import ngrams
+from parsinjson import combine_ngrams
+#from . import winrandom
 
 
 main_api = 'https://q4ktfaysw3.execute-api.us-east-1.amazonaws.com/treehacks/legislators?'
@@ -26,53 +29,63 @@ def bot_login():
     print('Logged in')
     return r
 
+r = bot_login()
+
 def run_bot(r, repliedto):
-    for comment in r.subreddit('contactyourrep').comments(limit=1000):
+    for comment in r.subreddit('contactyourrep').comments(limit=25):
     
         #if "San Francisco" in comment.body and comment.id not in repliedto and comment.author != r.user.me():
 
         #print(comment)
-        cbody=comment.body.split()
+        cbody=comment.body
+        cbody=ngrams(cbody, 2)
+        cbody=combine_ngrams(cbody)
+
 
 
         #print(cbody)
         for word in cbody:
-            print(word)
+            #print(word)
 
             if word in keyworddict:
+                print(word)
             
-        #if any(keyword in comment for keyword in keyworddict) and comment.id not in repliedto and comment.author !=r.user.me():
-        #if key in comment.body and comment.author !=r.user.me():
-                print("String found !")
-                with open ("repliedto.txt", "a") as f:
-                        f.write(comment.id + "\n")
-                text =str(comment.body)
-                #print(type(comment))
-                analysis = TextBlob(text)
-                print(analysis.sentiment[0])
-                if analysis.sentiment[0]<.3:
-                    address = keyworddict[word]
-                    url = main_api + urllib.parse.urlencode({'address': address})+urllib.parse.urlencode({'level': level})
-                    json_data = requests.get(url, headers=headers).json()
-                    contactdict = create_response(json_data)
-                    #print(contactdict)
+                #if comment.id not in repliedto and comment.author !=r.user.me():
+                if comment.author !=r.user.me():
+                    with open ("repliedto.txt", "a") as f:
+                            f.write(comment.id + "\n")
+                    text =str(comment.body)
+                    #print(type(comment))
+                    analysis = TextBlob(text)
+                    print(analysis.sentiment[0])
+                    if analysis.sentiment[0]<.4:
+                        address = keyworddict[word]
+                        print(address)
+                        url = main_api + urllib.parse.urlencode({'address': address})+urllib.parse.urlencode({'level': level})
+                        json_data = requests.get(url, headers=headers).json()
+                        #print(json_data)
+                        contactdict = create_response(json_data)
+                        #print(contactdict)
+                        #print(contactdict)
 
 
-                    replytext = ("Mad about what’s going on in your neighborhood? Don’t just complain on reddit, do something! Contact your representatives today! \n\n" + " ".join(str(x) for x in contactdict) + "Find scripts for issues you care about [Here](https://5calls.org/) \n\n" + "If you don’t actively participate in our democracy, Russian bots like me will win. \n\n **Brought to you by treehacks and Phone2action (phone2action.com).")
-                    #replytext = ("Mad about what’s going on in your neighborhood? Don’t just complain on reddit, do something! Contact your representative today!" + " ".join(str(x) for x in contactdict) + "Find scripts for issues you care about [Here](https://5calls.org/)" + "If you don’t actively participate in our democracy, Russian bots like me will win.  **Brought to you by treehacks and Phone2action (phone2action.com).")
-               
-                    #print(replytext)
-
-                
-                    comment.reply(replytext)
-                    print("replied to comment" + comment.id)
-                    #repliedto.append(comment.id)
+                        replytext = ("Mad about what’s going on in your neighborhood? Don’t just complain on reddit, do something! Contact your representatives today! \n\n" + " ".join(str(x) for x in contactdict) + "Find scripts for issues you care about [Here](https://5calls.org/) \n\n" + "If you don’t actively participate in our democracy, Russian bots like me will win. \n\n **Brought to you by treehacks and Phone2action (phone2action.com).")
+                        #replytext = ("Mad about what’s going on in your neighborhood? Don’t just complain on reddit, do something! Contact your representative today!" + " ".join(str(x) for x in contactdict) + "Find scripts for issues you care about [Here](https://5calls.org/)" + "If you don’t actively participate in our democracy, Russian bots like me will win.  **Brought to you by treehacks and Phone2action (phone2action.com).")
+                   
+                        print(replytext)
 
                     
+                        comment.reply(replytext)
+                        print("replied to comment" + comment.id)
+                        #repliedto.append(comment.id)
 
-        print("sleeping for a sec...")
-        time.sleep(1)
+                        
 
+            print("sleeping for a sec...")
+            time.sleep(5)
+
+
+#bot_login()
 def get_saved_comments():
     if not os.path.isfile("repliedto.txt"):
         repliedto = []
@@ -89,5 +102,4 @@ repliedto=get_saved_comments()
 print(repliedto)
 
 while True:
-    r = bot_login()
     run_bot(r, repliedto)
